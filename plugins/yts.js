5ab4fa31f2668b707c4c383d27c60dbc865af157
malinduaibot <nadithmalidu308@gmail.com>
2025-12-05 16:52:35 +0530

    Update yts.js

--- plugins/yts.js
+++ plugins/yts.js
@@ -48,7 +48,7 @@ ${formattedResults}
         from,
         {
           image: {
-            url: "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/yts.png?raw=true",
+            url: "https://github.com/malinduaibot/MALINDU-AI-BOT/blob/main/image/Screenshot%202025-12-05%20133333.png?raw=true",
           },
           caption,
         },
