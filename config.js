e4caa2126d661e91f5397487c4f5043855f69efd
malinduaibot <nadithmalidu308@gmail.com>
2025-12-05 17:30:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "HcVWmYib#Z7YCeqxe6AXMYN3plPqlu-xpqusU-_118N-wJDNIgAk",
+SESSION_ID: process.env.SESSION_ID || "jMsVVb5R#rc_FVuO9FE-wxp4RM9PNrpaKJ0Ffl0tUGgaUDKSVMSw",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/malinduaibot/MALINDU-AI-BOT/blob/main/image/Screenshot%202025-12-05%20133333.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 MALINDU AI BOT Is Alive Now😍*",
 BOT_OWNER: '94701369636',  // Replace with the owner's phone number
